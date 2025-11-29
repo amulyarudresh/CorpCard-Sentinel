814a9875620c36cafbc9988cfa6686094e258d29
amulyarudresh <amulyarudresh6666@gmail.com>
2025-11-29 17:10:41 +0530

    Fix database driver for Render

--- corpcard_sentinel/database.py
+++ corpcard_sentinel/database.py
@@ -8,6 +8,10 @@ load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:visa_secret@localhost:3306/corpcard_db")
 
+# Fix for Render: Force pymysql driver if default mysql:// is provided
+if SQLALCHEMY_DATABASE_URL.startswith("mysql://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("mysql://", "mysql+pymysql://")
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
